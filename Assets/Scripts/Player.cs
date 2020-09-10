a601034a727f648d149d6ed3c9f307abdae001c2
kevinhuart <huart.ke@gmail.com>
2020-09-10 12:41:03 +0200

    Tests de son
    Ajout de murs et de 2 sons avec les fonctionnalités de base de Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 movement;
+    public float movespeed = 5f;
+    public Transform camTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //déplacements
+        transform.Translate(movement * movespeed * Time.deltaTime);
+        camTransform.position = new Vector3(transform.position.x, transform.position.y, -5);
+    }
+}
