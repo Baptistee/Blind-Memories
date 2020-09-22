e59256420301943819a404d362674119db177280
BruccoleriAlois <lolo.brucco@gmail.com>
2020-09-22 10:10:17 +0200

    Acoustics

---
+++ Assets/Acoustics/Scripts/AcousticsNavigation.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace Microsoft.Cloud.Acoustics
+{
+    [DisallowMultipleComponent]
+    public class AcousticsNavigation : MonoBehaviour
+    {
+        // This component has no content. Its presence on a GameObject indicates
+        // that the object should be used as a navigation mesh when determining probe point layout.
+    }
+}
+#endif
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public NavMeshPath path;
+
+    void Start()
+    {
+        path = new NavMeshPath();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("if click");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("if move");
+                NavMesh.CalculatePath(agent.transform.position, hit.point,NavMesh.AllAreas,path);
+                // MOVE OUR AGENT
+                agent.SetDestination(hit.point);
+                for (int i = 0; i < path.corners.Length - 1; i++)
+                {
+                    Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red, 2, false);
+                }
+            }
+        }
+        
+    }
+}
