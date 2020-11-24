c2acf30ae5ecea7f21d6495596f6e3dc3bf13835
BruccoleriAlois <46890555+BruccoleriAlois@users.noreply.github.com>
2020-11-24 14:31:57 +0100

    The True and Only One Accoustics exemple

---
+++ Assets/Acoustics/Editor/AcousticsProbesRenderer.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.Cloud.Acoustics
+{
+    public class AcousticsProbesRenderer : AcousticsActualRenderer
+    {
+        private Triton.SimulationConfig m_previewData;
+
+        // Class that draws the gizmos showing the acoustic probe locations.
+        public override void Render()
+        {
+            if (m_previewData == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.cyan;
+
+            for (int curProbe = 0; curProbe < m_previewData.NumProbes; curProbe++)
+            {
+                Triton.Vec3f curLocation = m_previewData.GetProbePoint(curProbe);
+
+                Gizmos.DrawCube(AcousticsEditor.TritonToWorld(new Vector3(curLocation.x, curLocation.y, curLocation.z)), new Vector3(0.2f, 0.2f, 0.2f));
+            }
+        }
+
+        public void SetPreviewData(Triton.SimulationConfig results)
+        {
+            m_previewData = results;
+        }
+    }
+}
---
+++ Assets/Acoustics/Scripts/AcousticsGeometry.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR && !NET_4_6
+#error Microsoft Project Acoustics plugin requires "Scripting Runtime Version" and "API Compatibility Level" under Edit/Project Settings/Player set to ".NET 4.x". Please change the settings and restart the editor.
+#endif
+
+#if UNITY_EDITOR
+namespace Microsoft.Cloud.Acoustics
+{
+    [DisallowMultipleComponent]
+    public class AcousticsGeometry : MonoBehaviour
+    {
+        // This component has no content. Its presence on a GameObject indicates
+        // that the object should be used in cloud acoustics calculations.
+    }
+}
+#endif
