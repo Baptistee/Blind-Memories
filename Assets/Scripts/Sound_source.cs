a78b6857e8bb9b8597d2bc813828bd3a68c7be5e
kevinhuart <huart.ke@gmail.com>
2020-11-24 16:12:09 +0100

    Test d'affichage d'un souvenir lorsqu'on touche la source sonore

--- Assets/Scripts/Sound_source.cs
+++ Assets/Scripts/Sound_source.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sound_source : MonoBehaviour
 {
+    public Image souvenir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        souvenir.GetComponent<Image>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButton("Fire1"))
+        {
+            souvenir.GetComponent<Image>().enabled = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         GetComponent<AudioSource>().Stop();
+        souvenir.GetComponent<Image>().enabled = true;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
     }
 }
